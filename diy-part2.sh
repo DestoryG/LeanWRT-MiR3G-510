83af574d3c54b4c21abf77de282e7ab9c9ea19d1
DestoryG <136814177+DestoryG@users.noreply.github.com>
2024-01-29 13:33:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=5\.4/KERNEL_PATCHVER:=5.10/' target/linux/ramips/Makefile
 sed -i "s/set system.@system\[-1\].hostname='OpenWrt'/set system.@system[-1].hostname='DGWrt'/" package/base-files/files/bin/config_generate
+rm -rf package/feeds/routing/batman-adv
+mv files/batman-adv package/feeds/routing
