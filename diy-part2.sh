eea2935573637ebd052fc794c348539ad33e5874
DestoryG <136814177+DestoryG@users.noreply.github.com>
2025-01-18 11:58:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,5 @@ mv files/rustfix/Makefile package/feeds/packages/rust
 git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git packages
 sudo apt install libfuse-dev
+rm -rf feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
